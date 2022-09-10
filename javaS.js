bc8398b41b5dfcf8cba03d112b35a4d438a9fdd8
EthanGJFrench <ethanfrenc@gmail.com>
2022-09-11 11:42:00 +1200

    Links
    Have created link to youtube, I am now big brianed iPad Kids

--- javaS.js
+++ javaS.js
@@ -1,15 +1,4 @@
-//This is a variable
+var linkText = 'link to coolness';
+var url = 'https://www.youtube.com/';
 
-var example = 'I mad, I no talk to YOU!';
-
-//You can change variables in JS. If we want to change the var
-//example, beacuse is has already been decleared with the 'var'
-//keyword, we can change it like this...
-
-example = 'This is our JS code!';
-
-// This is our JS command. Is has been imported into our HTML 
-//doc and is being executed in our browser when doc is 
-//executed
-document.write(example);
-
+document.write('Click here to go to ' + linkText.link(url))
