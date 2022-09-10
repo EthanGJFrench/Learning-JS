b624e78aafd2f68591442093fe1042bba5ef8c71
EthanGJFrench <ethanfrenc@gmail.com>
2022-09-10 23:32:07 +1200

    Lesson 1 - 7

---
+++ javaS.js
@@ -0,0 +1,15 @@
+//This is a variable
+
+var example = 'I mad, I no talk to YOU!';
+
+//You can change variables in JS. If we want to change the var
+//example, beacuse is has already been decleared with the 'var'
+//keyword, we can change it like this...
+
+example = 'This is our JS code!';
+
+// This is our JS command. Is has been imported into our HTML 
+//doc and is being executed in our browser when doc is 
+//executed
+document.write(example);
+
