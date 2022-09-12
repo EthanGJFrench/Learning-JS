a234b6adcbe1989a19ee2014da37eddaa700c9e0
EthanGJFrench <ethanfrenc@gmail.com>
2022-09-12 15:16:04 +1200

    Update javaS.js

--- javaS.js
+++ javaS.js
@@ -1,4 +1,4 @@
-var linkText = 'link to coolness';
+
 var url = 'https://www.youtube.com/';
 
-document.write('Click here to go to ' + linkText.link(url))
+document.write(`<a href="${url}">Click here for some adult fun ;)</a>`)
