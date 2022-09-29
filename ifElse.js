857ab642582acdc4e4f1154e53c4e6028bc4ddb4
EthanGJFrench <ethanfrenc@gmail.com>
2022-09-29 22:19:58 +1300

    If and Else
    playing around with if statements

---
+++ ifElse.js
@@ -0,0 +1,9 @@
+
+var userName = prompt('What is your name?');
+
+if (userName) {
+    alert('Welcome ' + userName + ' !');
+} else {
+    alert('Welcome guest!');
+}
+
--- playingAround.js
+++ playingAround.js
@@ -14,8 +14,14 @@ var awnserString = String(driveAwnser);
 document.write('If you have been driving for ' + drive + ' ammount of years, you have been driving for ' + awnserString
     + ' days!');
 
-if (prompt('Is this information you have entered correct? (Type y/n)') == 'y') {
+var infoCheak = prompt('Is this information you have entered correct? (Type y/n)');
+var lowerInfoCheak = infoCheak.toLowerCase();
+
+if (lowerInfoCheak == 'y') {
     console.log('Thankyou for this accurate information');
 } else {
     console.log('This information is incorrect. Please refresh this page to retake this Quiz');
 }
+
+
+
