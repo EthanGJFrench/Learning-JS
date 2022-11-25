eb25c79298092b0959a3f8cde854d82f1a8f710a
EthanGJFrench <ethanfrenc@gmail.com>
2022-11-25 15:07:00 +1300

    html doc

--- README.md
+++
@@ -1,2 +0,0 @@
-# Learning-JS
-After every JS module completed, I will push a save. Think of this repository as notes! Also learning how to use GitHub. Bare with me!
--- playingAround.js
+++ playingAround.js
@@ -1,16 +1,10 @@
 
-// How to turn a string into and int
-
-// Getting string from user input. Saving the value to var drive
 var drive = prompt('How many years have you been driving for?');
 
-// Converting visitorAge variable to an Int. Saving value to driveAwnser 
 var driveAwnser = parseInt(drive) * 365;
 
-// Changing back to string(for fun/demonstration)
 var awnserString = String(driveAwnser);
 
-// Writing string onto webpage and combining the awnserString
 document.write('If you have been driving for ' + drive + ' ammount of years, you have been driving for ' + awnserString
     + ' days!');
 
